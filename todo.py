bed34165a6febcf9db8796d588734037f1f204c7
Vladimir Badashkin <vbadaskin@gamil.com>
2023-07-17 12:15:49 +0300

    Todo Update

--- todo.py
+++ todo.py
@@ -13,10 +13,9 @@ async def add_todo(todo: Todo) -> dict:
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
     return {"todos": todo_list}
-
+    
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = 
-            Path(..., title="The ID of the todo to retrive.")) -> dict:
+async def get_single_todo(todo_id: int = Path(..., title='The ID of the todo to retrive.')) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             return {
