08323ece9bb648d22fb87d7e7ca6bbca78072396
Vladimir Badashkin <vbadaskin@gamil.com>
2023-07-17 15:11:53 +0300

    Final changes in 2-nd chapter

--- model.py
+++ model.py
@@ -3,3 +3,21 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+    
+    class Config:
+        Schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
+        
+class TodoItem(BaseModel):
+    item: str
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read next charter of the book"
+            }
+        }
